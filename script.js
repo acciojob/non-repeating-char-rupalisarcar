bad6795499a28609b50b6b93d90fcfbf862909fd
rupalisarcar <rupalisarcar@gmail.com>
2025-08-29 22:18:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	if(str.length>0 && str.length<=1000){		
+		let freq={};
+		for(let char of str){
+			freq[char]=(freq[char]||0)+1;
+		}
+		console.log(freq)
+		for(let value in freq){
+			if(freq[value]==1)  return value;
+		}
+		return null;
+	
+	}else return null;	
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
